49d413d511586492de651e27b728e04639359e81
Davide Cioni <88600810+deivshon@users.noreply.github.com>
2023-08-06 12:55:07 +0200

    feat: :sparkles: add automatic browserleaks dns leak test script

---
+++ browser-leaks-run-dns-test/script.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         browser-leaks-run-dns-test
+// @namespace    https://github.com/deivshon
+// @version      1.0
+// @description  Automatically run the DNS test on browserleaks's IP page
+// @author       Davide Cioni
+// @match        https://browserleaks.com/ip
+// @grant        none
+// ==/UserScript==
+
+"use strict";
+
+const main = () => {
+    const dnsTestButton = document.getElementById("dns-run")
+    dnsTestButton.click()
+}
+
+main()
